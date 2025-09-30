fa94d367747d774948939f12a0d7312b13d97790
pmandries <pandries@aol.com>
2025-09-30 01:23:00 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const generateDomains = () => {
+    let pronoun = ['my', 'our', 'the', 'his', 'her'];
+    let adj1 = ['super', 'huge', 'tiny', 'unfathomable', 'monstrous'];
+    let adj2 = ['lit', 'sus', 'fun', 'boring', 'lazy'];
+    let noun = ['garbonzo', 'bassoon', 'sloth', 'koala', 'didgeridoo'];
+    let extension = ['.com', '.net', '.org', '.edu', '.gov'];
+
+    for(let i = 0; i < pronoun.length; i++) {
+      for(let j = 0; j < adj1.length; j++) {
+        for(let k = 0; k < adj2.length; k++) {
+          for(let l = 0; l < noun.length; l++) {
+            for(let m = 0; m < extension.length; m++) {
+              console.log("https://" + pronoun[i] + adj1[j] + adj2[k] + noun[l] + extension[m]);
+            }
+          }
+        }
+      }
+    }
+  }
+  generateDomains()
 };
+
